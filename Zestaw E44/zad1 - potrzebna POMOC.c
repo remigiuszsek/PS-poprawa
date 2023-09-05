f1f8c4aeccf611fb140f4c24c1f14202047c9176
Remi Sek <92035425+remigiuszsek@users.noreply.github.com>
2023-09-05 20:58:02 +0200

    Create zad1 - potrzebna POMOC.c

---
+++ Zestaw E44/zad1 - potrzebna POMOC.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void suma(int tab[][a], int a)
+{
+    printf("elo\n");
+}
+
+int main()
+{
+    int tab[1][2] = {{1, 2}};
+    int a = 2;
+    suma(tab, a);
+
+    return 0;
+}
