d568fecca55d43d2b08a1fc0d0591e132d3cc153
Remi Sek <92035425+remigiuszsek@users.noreply.github.com>
2023-09-06 09:57:45 +0200

    Update zad4.c

--- Zestaw P31/zad4.c
+++ Zestaw P31/zad4.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 struct node {
   int i;
   struct node * next;
 };
 
+int isSquare(int n)
+{
+    int i = 0;
+    while(i*i <= n)
+    {
+        if(i*i == n)
+        {
+            return 1;
+        }
+        i++;
+    }
+    
+    return 0;
+}
+
 void wyswietl(struct node * lista)
 {
     while(lista != NULL)
     {
-        if(sqrt(lista->i) ==  (int)sqrt(lista->i))
+        if(isSquare(lista->i))
         {
             printf("%d, ", lista->i);
         }
@@ -25,7 +39,7 @@ int main()
     
     lista->i = 4;
     lista->next = malloc(sizeof(struct node));
-    lista->next->i = 11;
+    lista->next->i = 0;
     lista->next->next = malloc(sizeof(struct node));
     lista->next->next->i = 25;
     lista->next->next->next = NULL;
